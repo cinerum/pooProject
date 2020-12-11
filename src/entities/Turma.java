712e8f3d6e1cbac615f538b8ddf72398e52ae00e
cinerum <54904248+cinerum@users.noreply.github.com>
2020-12-11 01:59:06 -0300

    Update Turma.java

--- src/entities/Turma.java
+++ src/entities/Turma.java
@@ -4,26 +4,28 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Scanner;
 
 public class Turma {
 
 	Scanner sc = new Scanner(System.in);
-	//STRING COD_TURMA
-	private String codigo_Turma;
+	private int codigo_Turma;
 	private Date hora_Inicial;
 	private Date hora_Final;
 	private Date data_Inicio;
 	private Date data_Fim;
 	private boolean status_Turma = true;
 	private Instrutor instrutor;
-	ArrayList<Aluno> aluno = new ArrayList<Aluno>();
+	private List<Aluno> alunos = new ArrayList<>();
+	//código do instrutor inserido pra poder setar na tabela turma o codigo do instrutor
+	private int cod_Instrutor;
 
 	public Turma() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public Turma(String codigo_Turma, Date hora_Inicial, Date hora_Final, Date data_Inicio, Date data_Fim,
+	public Turma(Integer codigo_Turma, Date hora_Inicial, Date hora_Final, Date data_Inicio, Date data_Fim,
 			Instrutor instrutor) {
 		this.codigo_Turma = codigo_Turma;
 		this.hora_Inicial = hora_Inicial;
@@ -31,25 +33,45 @@ public class Turma {
 		this.data_Inicio = data_Inicio;
 		this.data_Fim = data_Fim;
 		this.instrutor = instrutor;
+		ArrayList<Aluno> aluno = new ArrayList<Aluno>();
 	}
 
 	public void cadastrarTurma() throws ParseException {
-		System.out.println("CADASTRAR TURMA");
-		System.out.println("Codigo da Turma: ");
-		setCodigo_Turma(sc.next());
 
-		System.out.println("Data Inicio");
+		
+		System.out.println("HORA INICIAL");
+		String sHora1 = (sc.next());
+		SimpleDateFormat hFormat1 = new SimpleDateFormat("HH:mm:ss");
+		setHora_Inicial(hFormat1.parse(sHora1));
+
+		System.out.println("HORA FINAL");
+		String sHora2 = (sc.next());
+		SimpleDateFormat hFormat2 = new SimpleDateFormat("HH:mm:ss");
+		setHora_Final(hFormat2.parse(sHora2));
+		
+		System.out.println("DATA INICIO");
 		String sData = (sc.next());
-		SimpleDateFormat dFormat = new SimpleDateFormat("yyyy/MM/dd");
+		SimpleDateFormat dFormat = new SimpleDateFormat("dd/MM/yyyy");
 		setData_Inicio(dFormat.parse(sData));
+		
+		System.out.println("DATA FIM");
+		String sDataFim = (sc.next());
+		SimpleDateFormat dFormat1 = new SimpleDateFormat("dd/MM/yyyy");
+		setData_Fim(dFormat1.parse(sDataFim));
 
-	}
+		System.out.println("Código do Instrutor");
+		setCod_Instrutor(sc.nextInt());
+			
+			
+		}
+	
+	
 
-	public String getCodigo_Turma() {
+	public Integer getCodigo_Turma() {
 		return codigo_Turma;
 	}
 
-	public void setCodigo_Turma(String codigo_Turma) {
+	public void setCodigo_Turma(Integer codigo_Turma) {
 		this.codigo_Turma = codigo_Turma;
 	}
 
@@ -101,14 +123,21 @@ public class Turma {
 		this.instrutor = instrutor;
 	}
 
-	public ArrayList<Aluno> getAluno() {
-		return aluno;
+	public List<Aluno> getAlunos() {
+		return alunos;
 	}
 
-	public void setAluno(ArrayList<Aluno> aluno) {
-		this.aluno = aluno;
+	public int getCod_Instrutor() {
+		return cod_Instrutor;
 	}
-	
-	
 
+	public void setCod_Instrutor(int cod_Instrutor) {
+		this.cod_Instrutor = cod_Instrutor;
+	}
+
+	public void setAlunos(List<Aluno> alunos) {
+		this.alunos = alunos;
+	}
+
+	
 }
